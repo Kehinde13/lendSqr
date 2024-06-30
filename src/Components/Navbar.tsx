4db061a7696f974c1f03de9e0b351f42158cb37f
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-06-30 05:18:02 -0400

    styled user result

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,66 +1,72 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/Group (1).svg";
-import search from '../assets/Search.png'
-import alert from '../assets/alert.png'
-import userPic from '../assets/avatar.png'
-import dropdown from '../assets/Vector (2).png'
-import burger from '../assets/Menu.png'
-import '../Styles/Navbar.css'
+import search from "../assets/Search.png";
+import alert from "../assets/alert.png";
+import userPic from "../assets/avatar.png";
+import dropdown from "../assets/Vector (2).png";
+import burger from "../assets/Menu.png";
+import "../Styles/Navbar.css";
 import MobileMenu from "./MobileMenu";
 import { useState } from "react";
 import { userDataType } from "../Pages/Dashboard";
 import UserResult from "./UserResult";
 
 type Prop = {
-   userData: userDataType[] | undefined
-}
+  userData: userDataType[] | undefined;
+};
 
-const Navbar: React.FC<Prop> = ({userData}) => {
-  const [menu, setMenu] = useState(false)
-  const [user, setUser] = useState("")
-  const [userResult, setUserResult] = useState<userDataType | undefined>()
+const Navbar: React.FC<Prop> = ({ userData }) => {
+  const [menu, setMenu] = useState(false);
+  const [user, setUser] = useState("");
+  const [userResult, setUserResult] = useState<userDataType | undefined>();
 
   const searchData = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const result: userDataType | undefined = userData?.find((data) => data.username === user)
-    setUserResult(result)
-  }
+    e.preventDefault();
+    const result: userDataType | undefined = userData?.find(
+      (data) => data.username === user
+    );
+    setUserResult(result);
+  };
 
-  
-
- const toggleMenu = () => {
-    setMenu(!menu)
- }
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
   return (
     <div className="Navbar">
       <img src={logo} alt="logo" />
 
       <form onSubmit={searchData}>
-        <input type="text" placeholder="Search for user" onChange={(e) => setUser(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Search for user"
+          onChange={(e) => setUser(e.target.value)}
+        />
         <button>
-            <img src={search} alt="" />
+          <img src={search} alt="" />
         </button>
       </form>
 
-      {
-        userResult && <UserResult userResult={userResult} />
-      }
+      {userResult && (
+        <UserResult userResult={userResult} setUserResult={setUserResult} />
+      )}
 
       <div>
-        <Link to='dashboard' className="doc">Docs</Link>
+        <Link to="dashboard" className="doc">
+          Docs
+        </Link>
 
-        <img src={alert} alt="alert" className="alert"/>
+        <img src={alert} alt="alert" className="alert" />
 
-        <img src={burger} alt="" className="burger" onClick={toggleMenu}/>
+        <img src={burger} alt="" className="burger" onClick={toggleMenu} />
 
         <div className="user">
-            <img src={userPic} alt="user image" />
-            <p>Adedeji</p>
-            <img src={dropdown} alt="dropdown" className="dropdown"/>
+          <img src={userPic} alt="user image" />
+          <p>Adedeji</p>
+          <img src={dropdown} alt="dropdown" className="dropdown" />
         </div>
       </div>
 
-      <MobileMenu menu={menu} toggleMenu={toggleMenu}/>
+      <MobileMenu menu={menu} toggleMenu={toggleMenu} />
     </div>
   );
 };
--- src/Components/UserResult.tsx
+++ src/Components/UserResult.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { userDataType } from '../Pages/Dashboard';
-import '../Styles/UserResult.css'
+import React, { useEffect, useRef } from "react";
+import { userDataType } from "../Pages/Dashboard";
+import "../Styles/UserResult.css";
+import { Link } from "react-router-dom";
 
 type Prop = {
-    userResult: userDataType | undefined;
+  userResult: userDataType | undefined;
+  setUserResult: (userResult: userDataType | undefined) => void
 };
 
-const UserResult: React.FC<Prop> = ({ userResult }) => {
+const UserResult: React.FC<Prop> = ({ userResult, setUserResult }) => {
+ const resultRef = useRef<HTMLDivElement | null>(null)
+
+  const closeModal = () => {
+    setUserResult(undefined)
+  }
+
+  useEffect(() => {
+    const clickOutside = (e: MouseEvent) => {
+      if(resultRef.current && !resultRef.current.contains(e.target as Node)){
+        setUserResult(undefined)
+      }
+    };
+
+    document.addEventListener("mousedown", clickOutside)
+    return () => {
+      document.removeEventListener("mousedown", clickOutside)
+    }
+  })
+
   return (
-    <div className='userResult'>
-        {userResult?.username}
+    <div className="userResult" ref={resultRef}>
+      <Link to={`userdetails/${userResult?.id}`} onClick={closeModal}>
+          <p>{userResult?.organization}</p>
+          <p>{userResult?.username}</p>
+        <p
+          className={`${
+            userResult?.status === "Inactive"
+              ? "inactive"
+              : userResult?.status === "Active"
+              ? "active"
+              : userResult?.status === "Blacklisted"
+              ? "blacklisted"
+              : "pending"
+          }`}
+        >
+          {userResult?.status}
+        </p>
+      </Link>
     </div>
   );
 };
